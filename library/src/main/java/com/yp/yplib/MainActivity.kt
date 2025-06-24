8dc55096d50083724c1e9b8a930f09169fb3a8f0
86156 <wc0811@163.com>
2025-06-25 06:14:18 +0800

    init

--- library/src/main/java/com/yp/yplib/MainActivity.kt
+++ library/src/main/java/com/yp/yplib/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.yp.lib.lib.setLog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
