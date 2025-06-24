bec2580b74d545929c3d31a255928ecadb3b68b6
86156 <wc0811@163.com>
2025-06-25 05:56:34 +0800

    Add LogUtil

---
+++ app/src/main/java/com/yp/yplib/LogUtil.kt
@@ -0,0 +1,13 @@
+package com.yp.yplib
+
+import android.util.Log
+
+fun setLog(isLogAble: Boolean = true, tag: String? = "CWang", msg: String? = "") {
+    var message = "系统异常，请练习管理员"
+    msg?.let {
+        message = it
+    }
+    if (isLogAble) {
+        Log.v(tag, message)
+    }
+}
