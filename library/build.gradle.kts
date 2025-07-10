7b33774762ff556d3a806da4702bb64833d321e2
86156 <wc0811@163.com>
2025-07-11 02:08:00 +0800

    1.0.5

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -38,7 +38,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.github.wc0811"
                 artifactId = "YPLib"
-                version = "1.0.4"
+                version = "1.0.5"
             }
         }
     }
--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -45,5 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.github.wc0811:YPLib:1.0.3")
+    implementation(project(":library"))
 }
