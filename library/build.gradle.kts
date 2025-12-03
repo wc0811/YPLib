7543143471686ebfe47d5f3c141aff71dcec5a18
86156 <wc0811@163.com>
2025-12-04 06:49:41 +0800

    ver 1.0.1

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -37,7 +37,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.wc0811"
                 artifactId = "YPLib"
-                version = "1.0.0"
+                version = "1.0.1"
             }
         }
     }
